54e1c16c20855a53cbbf8c7708bbfd68ee57881a
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-01-26 22:31:37 -0500

    creando el componente de Material

---
+++ angular2-rss-reader-tutorial/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

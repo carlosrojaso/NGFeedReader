1c4b4f72bf951232da0f32c7ba9287a1a040c46d
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-01-28 21:36:34 -0500

    service

--- angular2-rss-reader-tutorial/src/app/feed.service.ts
+++ angular2-rss-reader-tutorial/src/app/feed.service.ts
@@ -23,8 +23,11 @@ export class FeedService {
     return feed || {};
   }
 
-  private handleError(error:any){
-    let errMsg= (error.message) ? error.message :
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 
 }
